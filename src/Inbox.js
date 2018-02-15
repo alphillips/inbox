b0f0a17a4489b820552874afff86eb3a876f1895
[zapjelly] <[pak.cindy@gmail.com]>
2018-02-15 17:35:40 +1100

    moving files

---
+++ src/Inbox.js
@@ -0,0 +1,195 @@
+import React from "react";
+import { Link } from "react-router";
+import PathwayList from "@react-ag-components/pathway-list";
+import * as api from "./api";
+import BackButton from "@react-ag-components/back-button";
+import Messages from "@react-ag-components/messages";
+import Mail from "./Mail";
+import moment from "moment";
+
+import "./inbox.css";
+
+class Inbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mails: {},
+      archives: {},
+      success: props.success,
+      error: props.error,
+      attachment: false,
+      showArchived: false,
+      messageId: null,
+      type: "",
+      showMail: false
+    };
+  }
+
+  componentDidMount() {
+
+    api.getMails().then(data => {
+      this.setState((prevState, props) => ({
+        mails: data
+      }));
+    });
+
+    api.getArchived().then(data => {
+      this.setState((prevState, props) => ({
+        archives: data
+      }));
+    });
+  }
+
+  epochSecondToDate = epochSecond => {
+    var eps = epochSecond * 1000;
+    var m = moment(eps);
+    var s = m.format("D/M/YYYY hh:mm:ss");
+    return s;
+  };
+
+  showArchived = () => {
+    this.setState((prevState, props) => ({
+      showArchived: !this.state.showArchived,
+      success: ""
+    }));
+  };
+
+  setupMail = (id, type) => {
+    this.setState((prevState, props) => ({
+      messageId: id,
+      type: type,
+      showMail: true
+    }));
+  };
+
+  callbackCloseSelf = () => {
+    this.setState((prevState, props) => ({
+      showMail: false
+    }));
+  };
+  callbackSetMessage = (type,msg) => {
+    this.setState((prevState, props) => ({
+      [type]:msg
+    }));
+  }
+
+
+  render() {
+
+    var mailNode = null;
+    if (this.state.showMail){
+      mailNode = (
+        <Mail
+        messageId={this.state.messageId}
+        type={this.state.type}
+        callbackCloseSelf={this.callbackCloseSelf}
+        callbackSetMessage={this.callbackSetMessage} />
+      )
+    }
+
+    return (
+      <div className="nexdoc-inbox">
+        {!this.state.showMail && (
+          <div>
+            <BackButton />
+            <Messages success={this.state.success} />
+
+            <h1>
+              {!this.state.showArchived && <span>Inbox</span>}{" "}
+              {this.state.showArchived && <span>Archived</span>}
+            </h1>
+            <Link onClick={this.showArchived} className="btn-archived">
+              View {!this.state.showArchived && <span>Archived</span>}{" "}
+              {this.state.showArchived && <span>Inbox</span>}
+            </Link>
+
+              {this.state.mails &&
+                this.state.mails.length > 0 && (
+                  <div className="inbox">
+                    <PathwayList>
+                      {!this.state.showArchived &&
+                        this.state.mails.map(mail => (
+                          <li
+                            className={
+                              "inbox-listing " + (!mail.read ? "unread" : "")
+                            }
+                            key={mail.messageId + mail.fromParty}
+                          >
+                            <div className="border-unread" />
+                            <Link
+                              onClick={this.setupMail.bind(
+                                null,
+                                mail.messageId,
+                                mail.type
+                              )}
+                            >
+                              <span className="inbox-date">
+                                {this.epochSecondToDate(
+                                  mail.messageTimestamp.epochSecond
+                                )}
+                              </span>
+                              {this.state.attachment && (
+                                <span className="inbox-attachment" />
+                              )}
+                              <span className="inbox-from">
+                                {mail.fromParty}
+                              </span>
+                              <span className="inbox-subject">
+                                {mail.subject}
+                              </span>
+                              <span className="inbox-body">
+                                {mail.body.replace(/<\/?[^>]+(>|$)/g, "")}
+                              </span>
+                            </Link>
+                          </li>
+                        ))}
+                      {this.state.showArchived &&
+                        this.state.archives.map(mail => (
+                          <li
+                            className={
+                              "inbox-listing " + (!mail.read ? "unread" : "")
+                            }
+                            key={mail.messageId + mail.fromParty}
+                          >
+                            <div className="border-unread" />
+                            <Link
+                              onClick={this.setupMail.bind(
+                                null,
+                                mail.messageId,
+                                mail.type
+                              )}
+                            >
+                              <span className="inbox-date">
+                                {this.epochSecondToDate(
+                                  mail.messageTimestamp.epochSecond
+                                )}
+                              </span>
+                              {this.state.attachment && (
+                                <span className="inbox-attachment" />
+                              )}
+                              <span className="inbox-from">
+                                {mail.fromParty}
+                              </span>
+                              <span className="inbox-subject">
+                                {mail.subject}
+                              </span>
+                              <span className="inbox-body">
+                                {mail.body.replace(/<\/?[^>]+(>|$)/g, "")}
+                              </span>
+                            </Link>
+                          </li>
+                        ))}
+                    </PathwayList>
+                  </div>
+                )}
+          </div>
+        )}
+
+        {this.state.showMail && (
+          mailNode
+        )}
+      </div>
+    );
+  }
+}
+export default Inbox;
---
+++ src/Mail/Mail.js
@@ -0,0 +1,304 @@
+import React from 'react'
+import * as api from './../api'
+import Dropzone from 'react-dropzone'
+import Messages from '@react-ag-components/messages'
+import moment from 'moment'
+
+
+import './mail.css'
+
+class Mail extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      mails: [],
+      success:props.success,
+      error:props.error,
+      id: -1,
+      messageId: parseInt(props.messageId) || -1,
+      type: props.type || "MESSAGE",
+      reply: '',
+      replyState: false,
+      showAttach: false,
+      html: '',
+      subject: '',
+      user: null,
+      files: [],
+      read: false,
+      archived: false,
+      info: ""
+    }
+    this.replies = []
+    this.handleReplyContent = this.handleReplyContent.bind(this)
+  }
+
+  componentDidMount() {
+
+    let expiryDate = "31/01/2018"
+    this.setState((prevState, props) => ({
+      info: "This notification will expire on " + expiryDate + "."
+    }))
+
+    if(this.state.type === "MESSAGE") {
+      api.getMail(this.state.messageId).then(
+        data => {
+          let threadMessage = data.linkedMessage
+          threadMessage.push(data)
+          this.setState((prevState, props) => ({
+            mails: threadMessage,
+            read: data.read,
+            archived: data.archived,
+            id: data.messageId,
+            subject: data.subject
+          }))
+
+          if(!this.state.read) {
+            let statusBody = {}
+            statusBody.messageId = this.state.id
+            statusBody.status = true
+          }
+        }
+      )
+    }
+    else {
+      api.getMailFromAll(this.state.messageId).then(
+        data => {
+          var threadMessage = []
+          threadMessage.push(data)
+          this.setState((prevState, props) => ({
+            mails: threadMessage,
+            read: data.read,
+            archived: data.archived,
+            id: data.messageId,
+            subject: data.subject
+          }))
+
+          if(!this.state.read) {
+            let statusBody = {}
+            statusBody.messageId = this.state.id
+            statusBody.status = true
+          }
+        }
+      )
+    }
+  }
+
+  handleReplyState = () => {
+    this.setState((prevState, props) => ({
+      replyState: !this.state.replyState,
+      showAttach: false
+    }))
+    this.clearAttachment()
+    this.clearReplyContent()
+  }
+
+  handleReplyContent = (event) => {
+    this.setState({html: event.target.value})
+  }
+
+  handleShowAttach = () => {
+    this.setState((prevState, props) => ({
+      showAttach: !this.state.showAttach
+    }))
+    if(!this.state.showAttach) {
+      this.clearAttachment()
+    }
+  }
+
+  handleSend = () => {
+    let reply = {}
+
+    reply.subject = this.state.subject.indexOf("Re:") > -1 ? this.state.subject :"Re: "+ this.state.subject
+    reply.body = this.state.html
+    reply.linkedAttachment = this.state.files
+    reply.parentId = this.state.id
+
+    api.sendMail(reply).then(
+      data => {
+        this.props.callbackSetMessage("success", 'Message '+'"'+  this.state.subject +'"'+ ' has been sent'),
+        this.props.callbackCloseSelf()
+      }
+    )
+  }
+
+  clearAttachment = () => {
+    this.setState((prevState, props) => ({
+      files: []
+    }))
+  }
+
+  clearReplyContent = () => {
+    this.setState({html: ''})
+  }
+
+  handleArchive = (e) => {
+    let statusBody = {}
+    statusBody.messageId = this.state.id
+    statusBody.archived = true
+    api.setArchive(statusBody).then(
+      data => {
+        this.props.callbackSetMessage("success",  'Message '+'"'+  this.state.mails[0].subject +'"'+ ' has been archived')
+        this.props.callbackCloseSelf()
+      }
+    ).catch((error) => {
+      console.log(error + " with archiving mail")
+    })
+  }
+
+  handleRestore = (e) => {
+    let statusBody = {}
+    statusBody.messageId = this.state.id
+    statusBody.archived = false
+    api.setArchive(statusBody).then(
+      data => {
+        this.props.callbackSetMessage("success", 'Message '+'"'+  this.state.mails[0].subject +'"'+ ' has been unarchived')
+        this.props.callbackCloseSelf()
+      }
+    ).catch((error) => {
+      console.log(error + " with restoring mail")
+    })
+  }
+
+  onDrop = (files) => {
+    files.forEach(file => {
+      const reader = new FileReader()
+      const fileObj = {}
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        fileObj.name = file.name
+        fileObj.mimeType = file.type
+        fileObj.size = this.convertFromBytetoMB(file.size)
+        fileObj.data = unescape(encodeURIComponent(reader.result.split(',')[1]))
+        let thisStateFiles = this.state.files
+        thisStateFiles.push(fileObj)
+        this.setState((prevState, props) => ({
+          files: thisStateFiles
+        }))
+      }
+    })
+  }
+
+  removeFile = (e) => {
+    var array = this.state.files;
+    var index = e.target.getAttribute('data-value')
+    array.splice(index, 1);
+    this.setState((prevState, props) => ({
+      files: array
+    }))
+  }
+  getfile = (e) => {
+    // retrive file from server and downloads it
+  }
+
+  convertFromBytetoMB = (num) => {
+    return (num/1048576).toFixed(4);
+  }
+
+  epochSecondToDate = (epochSecond) => {
+    var eps = epochSecond * 1000
+    var m = moment(eps)
+    var s = m.format("D/M/YYYY hh:mm:ss")
+    return s
+  }
+
+  onClose = () => {
+    this.props.callbackCloseSelf()
+    this.props.callbackSetMessage("success", "")
+  }
+
+  render() {
+    return (
+    <div>
+      <a className="back-button uikit-direction-link uikit-direction-link--left" onClick={this.onClose}> Back </a>
+
+      <div className="" className={this.state.replyState ? "nexdoc-mail-container reply" : "nexdoc-mail-container"}>
+
+        <div className="nexdoc-mail">
+          <textarea value={this.state.html} onChange={this.handleReplyContent} className={this.state.replyState? "reply-area reply" : "reply-area"}></textarea>
+
+          {this.state.showAttach &&
+            <div>
+              <Dropzone
+                onDrop={this.onDrop.bind(this)}
+                className="file-drop-zone">
+                {this.state.name ?
+                  <p>File <strong>{this.state.name}</strong> selected</p>
+                  :
+                  <p>Select or drag and drop a file</p>
+                }
+              </Dropzone>
+              {this.state.files.length > 0 &&
+                <aside className="attach-files">
+                  <h2>Attaching files</h2>
+                   <ul>
+                     {
+                       this.state.files.map((f, i) =>
+                          <li key={f.name+f.size} data-value={i} onClick={this.removeFile.bind(this)} key={f.name}>{f.name} - {f.size} MB</li>
+                        )
+                     }
+                   </ul>
+               </aside>
+               }
+             </div>
+           }
+
+          {this.state.mails &&
+
+          this.state.mails.map((reply) =>
+
+            <div key={reply.messageId}>
+
+              <div className="mail-from">From: <span className="text-normal">{reply.fromParty}</span>
+                {!this.state.replyState &&
+                  <div>
+                    {this.state.type === "MESSAGE" &&
+                      <div>
+                        {!this.state.archived &&
+                          <button className="btn-reply-action reply" onClick={this.handleReplyState}>Reply</button>
+                        }
+
+                        {!this.state.archived &&
+                          <button className="btn-archive" onClick={this.handleArchive}>Archive</button>
+                        }
+                        {this.state.archived &&
+                          <button className="btn-unarchive" onClick={this.handleRestore}>Unarchive</button>
+                        }
+                      </div>
+                    }
+                  </div>
+                }
+                {this.state.replyState &&
+                  <div>
+                    <button className="btn-reply-action discard" onClick={this.handleReplyState}>Discard</button>
+                    <button className="btn-reply-action send" onClick={this.handleSend}>Send</button>
+                    <button className="btn-reply-action attach" onClick={this.handleShowAttach}>{!this.state.showAttach ? 'Attach' : 'Remove'}</button>
+                  </div>
+                }
+              </div>
+              <div className="mail-subject-container">
+                <span className="mail-subject">Subject: <span className="text-normal">{reply.subject}</span></span>
+                <span className="mail-date">Sent: <span className="text-normal">{this.epochSecondToDate(reply.messageTimestamp.epochSecond)}</span></span>
+                {this.state.type !== "MESSAGE" &&
+                  <span className="mail-date">Notice: <span className="text-normal">{this.state.info}</span></span>
+                }
+
+                {this.state.type === "MESSAGE" && reply.linkedAttachment &&
+                  <span className="mail-date">Attachment: <span className="text-normal">{reply.linkedAttachment.map((file, i) =>  file.name + " " )}</span></span>
+                }
+              </div>
+              <div className="mail-body-container"><span className="mail-body" dangerouslySetInnerHTML={{__html: (reply.body)}}></span></div>
+              {
+                this.state.mails.linkedAttachment &&
+                <button className="mail-attachment" data-value={reply.linkedAttachment.name} onClick={this.getfile.bind(this)}><span>{reply.linkedAttachment.name}</span></button>
+              }
+            </div>
+          )}
+        </div>
+
+      </div>
+    </div>
+    )
+  }
+}
+export default Mail
